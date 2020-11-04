b7cba82750f5985825ad312aa70a86751ee5d1cc
Tumpa sheet <68402283+sTumpaGit@users.noreply.github.com>
2020-11-04 14:27:57 +0530

    Create main_calculator.java
    main class

---
+++ src/main_calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class main_calculator {
+
+	public static void main(String[] args) {
+		
+		try {
+		calculator cal = new calculator();
+	}catch(Exception e) {
+		System.out.print(e);
+	}
+  
+ }
+}
